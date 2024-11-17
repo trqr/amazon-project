b818b689689135e14b96b38a4c4d1e228eea7d52
trqr <tristanqr@gmail.com>
2024-11-17 14:35:46 +0100

    generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,20 +1,26 @@
-export const cart = [];
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+}, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}];
 
 export function addToCart(productId) {
     let matchingItem;
-  
+
     cart.forEach((cartItem) => {
-      if (productId === cartItem.productId) {
-        matchingItem = cartItem;
-      }
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
     });
-  
+
     if (matchingItem) {
-      matchingItem.quantity += +document.querySelector(`.js-quantity-selector-${productId}`).value
+        matchingItem.quantity += +document.querySelector(`.js-quantity-selector-${productId}`).value
     } else {
-      cart.push({
-        productId,
-        quantity: +document.querySelector(`.js-quantity-selector-${productId}`).value
-      });
+        cart.push({
+            productId,
+            quantity: +document.querySelector(`.js-quantity-selector-${productId}`).value
+        });
     }
-  }
+}
